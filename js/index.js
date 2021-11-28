56ef7ec287531888964d4b6218303655c65c0e9b
admin <admin@qq.com>
2021-11-28 12:05:06 +0800

    优化了一下搜索的问题

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // 搜索
 $('#btn').on('click', function () {
-  window.location.href =
-    'https://www.sogou.com/web?ie=UTF-8&query=' + $('#ipt').val()
+  window.location.href ='https://www.sogou.com/web?ie=UTF-8&query=' + $('#ipt').val()   
+  $('#ipt').val()=''
 })
 // 其他
 console.log('以后有可能会完善')
