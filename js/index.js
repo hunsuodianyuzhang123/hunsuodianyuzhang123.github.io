b51065f5fb7fa2745a48440993b83d469be052cb
admin <admin@qq.com>
2021-11-28 11:54:03 +0800

    增加了搜索框

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+// 搜索
+$('#btn').on('click', function () {
+  window.location.href =
+    'https://www.sogou.com/web?ie=UTF-8&query=' + $('#ipt').val()
+})
 // 其他
 console.log('以后有可能会完善')
 console.log('随便写的，有什么问题的话可别找我哦~~')
